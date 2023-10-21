cbf5ddcb309605dac98cf6828369c3d0bbb86c78
Mingchung Xia <mingchung.xia@gmail.com>
2023-10-21 02:21:37 -0400

    Finished financial assistant

--- cardmaster/Source/Features/Assistant/ViewModels/AssistantViewModel.swift
+++ cardmaster/Source/Features/Assistant/ViewModels/AssistantViewModel.swift
@@ -6,13 +6,28 @@
 //
 
 import Foundation
+import Alamofire
 
 @MainActor
 final class AssistantViewModel: ObservableObject {
     @Published var messages: [String] = []
     
     func respond(msg: String) async {
+        let endpoint: URLConvertible = "http://127.0.0.1:8001/respond"
+        let parameters: Parameters = [
+            "msg": msg
+        ]
         self.messages.append(msg)
-        self.messages.append("RESPONSE")
+        AF.request(endpoint, parameters: parameters).responseData { response in
+            switch response.result {
+            case .success(let data):
+                if let json = try? JSONSerialization.jsonObject(with: data, options: []), let jsonDict = json as? [String: Any] {
+                    let reply = jsonDict["response"] as? String
+                    self.messages.append(reply ?? "")
+                }
+            case .failure(_):
+                debugPrint("Failed")
+            }
+        }
     }
 }
